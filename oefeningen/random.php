3d5fdde27c1b7852ec69b4c031f5dc62e02f21c3
Maurits Seelen <mauritsseelen@gmail.com>
2018-10-15 10:23:40 +0200

    ..

--- oefeningen/random.php
+++ oefeningen/random.php
@@ -1,6 +1,14 @@
 <?php
 
-$rand = rand(0,5);
-echo $rand;
+$randomCharacter = array();
+
+for ($i = 0; $i < 5; $i++) {
+    $rand = rand(65, 90);
+    array_push($randomCharacter, chr($rand));
+}
+
+foreach ($randomCharacter as $char) {
+    echo $char . "<br>";
+}
 
 ?>
