67ccb2706d2fc3a717098035757b575cc7f9b144
Maurits Seelen <mauritsseelen@gmail.com>
2018-12-23 11:58:36 +0100

    redone layout

--- dag-rapport/header.php
+++ dag-rapport/header.php
@@ -11,8 +11,10 @@
     <title>Eyewitness - dagrapport</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
@@ -22,4 +24,4 @@
     <script src="datatables.js"></script>
   </head>
 
-  <body id="page-top">
+  <body class="w3-black">
--- dag-rapport/index.php
+++ dag-rapport/index.php
@@ -1,83 +1,80 @@
 <?php
 include 'header.php';
 ?>
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-      <div class="container">
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#rapport">Rapport</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="overzicht.php">Overzicht</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="artikelen.php">Verkochte artikelen</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+<nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
+  <!-- Avatar image in top left corner -->
+  <img src="img/eyewitness.png" style="width:100%">
+  <a href="index.php" class="w3-bar-item w3-button w3-padding-large w3-black">
+    <i class="fa fa-home w3-xxlarge"></i>
+    <p>RAPPORT</p>
+  </a>
+  <a href="overzicht.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+    <i class="fa fa-user w3-xxlarge"></i>
+    <p>OVERZICHT RAPPORTEN</p>
+  </a>
+  <a href="artikelen.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+    <i class="fa fa-eye w3-xxlarge"></i>
+    <p>VERKOCHTE ARTIKELEN</p>
+  </a>
+</nav>
 
-<div class="context">
-    <!-- Header -->
-    <header class="masthead">
-      <div class="container d-flex h-100 align-items-center">
-        <div class="mx-auto text-center">
-          <h1 class="mx-auto my-0 text-uppercase">Dag rapport <?php echo date('d-m-Y');?></h1>
-          <h2 class="text-white-50 mx-auto mt-2 mb-5"></h2>
-        </div>
-      </div>
-    </header>
+<!-- Navbar on small screens (Hidden on medium and large screens) -->
+<div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
+  <div class="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
+    <a href="index.php" class="w3-bar-item w3-button" style="width:25% !important">RAPPORT</a>
+    <a href="overzicht.php" class="w3-bar-item w3-button" style="width:25% !important">OVERZICHT RAPPORTEN</a>
+    <a href="verkochte_artikelen.php" class="w3-bar-item w3-button" style="width:25% !important">VERKOCHTE ARTIKELEN</a>
+  </div>
+</div>
+<div class="w3-padding-large" id="main">
+  <!-- Header/Home -->
+  <header class="w3-container w3-padding-32 w3-center w3-black" id="home">
+    <h1 class="w3-jumbo"><span class="w3-hide-small">Eyewitness</span> dagrapport</h1>
+    <p><?php echo date('d:m:Y');?></p>
+    <img src="img/eyewitness.png" alt="Eyewitness" class="w3-image" width="280" height="140">
+  </header>
 
-    <!-- Rapport invullen -->
-    <section id="rapport" class="about-section text-center">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 mx-auto">
-              <form method="post" action="process.php" id="rapportForm">
-                <div class="form-group">
-                  <label for="Dag">Dag</label>
-                  <input type="text" class="form-control" id="dag" placeholder="dag" name="dag">
-                </div>
-                <div class="form-group">
-                  <label for="datum">Datum</label>
-                  <input type="date" value ="<?php echo date('Y-m-d');?>" class="form-control" id="datum" placeholder="datum" name="datum">
-                </div>
-                <div class="form-group">
-                  <label for="aantal">Aantal bezoekers</label>
-                  <input type="text" class="form-control" id="aantal" placeholder="aantal bezoekers" name="aantal">
-                </div>
-                <div class="form-group">
-                <label  for="aantal">Omzet</label>
-                  <input type="text" class="form-control" id="omzet" placeholder="omzet" name="omzet">
-                </div>
-                <div class="form-group">
-                  <label for="telefoon">Telefoon boodschappen (naam + telnr vermelden)</label>
-                  <textarea class="form-control" id="telefoon" rows="3" name="telefoon"></textarea>
-                </div>
-                <div class="form-group">
-                  <label  for="bijzonderheden">Bijzonderheden</label>
-                  <textarea class="form-control" id="bijzonderheden" rows="3" name="bijzonderheden"></textarea>
-                </div>
-                <div class="form-group artikelen">
-                  <label  for="bijzonderheden">Verkochte museumshop artikelen</label>
-                  <input type="text" class="form-control" id="verkochte1" placeholder="artikel naam" name="verkochte1">
-                  <input type="text" class="form-control" id="bedrag1" placeholder="bedrag" name="bedrag1">
-                  <span>Gepind?</span><input type="checkbox" class="form-control" id="gepind1" name="gepind1">
-                </div>
-                
-                <input type="hidden" name="aantalArtikelen" id="aantalArt">
-
-                <div class="form-group">                  
-                <button type="button" class="btn btn-default" onclick="addArtikelFields()">Voeg extra artikel veld toe</button>
-                  <input type="submit" class="btn btn-default" onclick="getAantalVerkochteArtikelen()">
-                </div>
-              </form>
-          </div>
-        </div>
-        <img src="img/demo-image-02.jpg" class="img-fluid" alt="">
+  <!-- About Section -->
+  <div class="w3-content w3-justify w3-text-grey w3-padding-64" id="about">
+    <h2 class="w3-text-light-grey">Rapport</h2>
+    <hr style="width:200px" class="w3-opacity">
+      <form method="post" action="process.php" id="rapportForm">
+      <div class="form-group">
+        <label for="Dag">Dag</label>
+        <input type="text" class="form-control" id="dag" placeholder="dag" name="dag">
       </div>
-    </section>
-    </div>
+      <div class="form-group">
+        <label for="datum">Datum</label>
+        <input type="date" value ="<?php echo date('Y-m-d');?>" class="form-control" id="datum" placeholder="datum" name="datum">
+      </div>
+      <div class="form-group">
+        <label for="aantal">Aantal bezoekers</label>
+        <input type="text" class="form-control" id="aantal" placeholder="aantal bezoekers" name="aantal">
+      </div>
+      <div class="form-group">
+      <label  for="aantal">Omzet</label>
+        <input type="text" class="form-control" id="omzet" placeholder="omzet" name="omzet">
+      </div>
+      <div class="form-group">
+        <label for="telefoon">Telefoon boodschappen (naam + telnr vermelden)</label>
+        <textarea class="form-control" id="telefoon" rows="3" name="telefoon"></textarea>
+      </div>
+      <div class="form-group">
+        <label  for="bijzonderheden">Bijzonderheden</label>
+        <textarea class="form-control" id="bijzonderheden" rows="3" name="bijzonderheden"></textarea>
+      </div>
+      <div class="form-group artikelen">
+        <label  for="bijzonderheden">Verkochte museumshop artikelen</label>
+        <input type="text" class="form-control" id="verkochte1" placeholder="artikel naam" name="verkochte1">
+        <input type="text" class="form-control" id="bedrag1" placeholder="bedrag" name="bedrag1">
+        <span>Gepind?</span><input type="checkbox" class="form-control" id="gepind1" name="gepind1">
+      </div>
+      
+      <input type="hidden" name="aantalArtikelen" id="aantalArt">
+
+      <div class="form-group">                  
+      <button type="button" class="btn btn-default" onclick="addArtikelFields()">Voeg extra artikel veld toe</button>
+        <input type="submit" class="btn btn-default" onclick="getAantalVerkochteArtikelen()">
+      </div>
+    </form>
+  </div
