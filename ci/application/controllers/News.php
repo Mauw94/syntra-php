b4c604745e67efc0f6412a16f42f410367b6517c
Maurits Seelen <mauritsseelen@gmail.com>
2019-02-08 16:35:46 +0100

    geen idee lol

--- ci/application/controllers/News.php
+++ ci/application/controllers/News.php
@@ -1,87 +1,220 @@
 <?php
-class News extends CI_Controller {
 
-        public function __construct()
-        {
-                parent::__construct();
-                $this->load->model('news_model');
-                $this->load->helper('url_helper');
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class News extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('News_model');
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $q = urldecode($this->input->get('q', TRUE));
+        $start = intval($this->input->get('start'));
+        
+        if ($q <> '') {
+            $config['base_url'] = base_url() . 'news/index.html?q=' . urlencode($q);
+            $config['first_url'] = base_url() . 'news/index.html?q=' . urlencode($q);
+        } else {
+            $config['base_url'] = base_url() . 'news/index.html';
+            $config['first_url'] = base_url() . 'news/index.html';
         }
 
-        public function index()
-        {
-                $data['news'] = $this->news_model->get_news();
-                $data['title'] = 'News archive';
+        $config['per_page'] = 10;
+        $config['page_query_string'] = TRUE;
+        $config['total_rows'] = $this->News_model->total_rows($q);
+        $news = $this->News_model->get_limit_data($config['per_page'], $start, $q);
 
-                $this->load->view('templates/header', $data);
-                $this->load->view('news/index', $data);
-                $this->load->view('templates/footer');
-            }
+        $this->load->library('pagination');
+        $this->pagination->initialize($config);
 
-            public function view($slug = NULL)
-            {
-                    $data['news_item'] = $this->news_model->get_news($slug);
-            
-                    if (empty($data['news_item']))
-                    {
-                            show_404();
-                    }
-            
-                    $data['title'] = $data['news_item']['title'];
-            
-                    $this->load->view('templates/header', $data);
-                    $this->load->view('news/view', $data);
-                    $this->load->view('templates/footer');
-            }
+        $data = array(
+            'news_data' => $news,
+            'q' => $q,
+            'pagination' => $this->pagination->create_links(),
+            'total_rows' => $config['total_rows'],
+            'start' => $start,
+        );
+        $this->load->view('news/news_list', $data);
+    }
 
-            public function delete($slug  = NULL)
-            {
-                $this->news_model->delete_news($slug);
-                $this->load->view('news/success');                 
-            }
+    public function read($id) 
+    {
+        $row = $this->News_model->get_by_id($id);
+        if ($row) {
+            $data = array(
+		'id' => $row->id,
+		'title' => $row->title,
+		'slug' => $row->slug,
+		'text' => $row->text,
+	    );
+            $this->load->view('news/news_read', $data);
+        } else {
+            $this->session->set_flashdata('message', 'Record Not Found');
+            redirect(site_url('news'));
+        }
+    }
 
-            public function update($slug = NULL)
-            {
-                $this->load->helper('form');
-                $this->load->library('form_validation');
-            
-                $data['title'] = 'Update news item';
-                
-                $this->form_validation->set_rules('title', 'Title', 'required');
-                $this->form_validation->set_rules('text', 'Text', 'required');
+    public function create() 
+    {
+        $data = array(
+            'button' => 'Create',
+            'action' => site_url('news/create_action'),
+	    'id' => set_value('id'),
+	    'title' => set_value('title'),
+	    'slug' => set_value('slug'),
+	    'text' => set_value('text'),
+	);
+        $this->load->view('news/news_form', $data);
+    }
+    
+    public function create_action() 
+    {
+        $this->_rules();
 
-                if ($this->form_validation->run() === FALSE)
-                {
-                        $data['news_item'] = $this->news_model->get_news($slug);
-                        $this->load->view('news/update', $data);        
-                } else {
-                        $this->news_model->update_news();
-                        $this->load->view('news/success');
-                }
-                
-            }
+        if ($this->form_validation->run() == FALSE) {
+            $this->create();
+        } else {
+            $data = array(
+		'title' => $this->input->post('title',TRUE),
+		'slug' => $this->input->post('slug',TRUE),
+		'text' => $this->input->post('text',TRUE),
+	    );
 
-            public function create()
-            {
-                $this->load->helper('form');
-                $this->load->library('form_validation');
-            
-                $data['title'] = 'Create a news item';
-            
-                $this->form_validation->set_rules('title', 'Title', 'required');
-                $this->form_validation->set_rules('text', 'Text', 'required');
-            
-                if ($this->form_validation->run() === FALSE)
-                {
-                    $this->load->view('templates/header', $data);
-                    $this->load->view('news/create');
-                    $this->load->view('templates/footer');
-            
-                }
-                else
-                {
-                    $this->news_model->set_news();
-                    $this->load->view('news/success');
-                }
-            }     
+            $this->News_model->insert($data);
+            $this->session->set_flashdata('message', 'Create Record Success');
+            redirect(site_url('news'));
+        }
+    }
+    
+    public function update($id) 
+    {
+        $row = $this->News_model->get_by_id($id);
+
+        if ($row) {
+            $data = array(
+                'button' => 'Update',
+                'action' => site_url('news/update_action'),
+		'id' => set_value('id', $row->id),
+		'title' => set_value('title', $row->title),
+		'slug' => set_value('slug', $row->slug),
+		'text' => set_value('text', $row->text),
+	    );
+            $this->load->view('news/news_form', $data);
+        } else {
+            $this->session->set_flashdata('message', 'Record Not Found');
+            redirect(site_url('news'));
+        }
+    }
+    
+    public function update_action() 
+    {
+        $this->_rules();
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->update($this->input->post('id', TRUE));
+        } else {
+            $data = array(
+		'title' => $this->input->post('title',TRUE),
+		'slug' => $this->input->post('slug',TRUE),
+		'text' => $this->input->post('text',TRUE),
+	    );
+
+            $this->News_model->update($this->input->post('id', TRUE), $data);
+            $this->session->set_flashdata('message', 'Update Record Success');
+            redirect(site_url('news'));
+        }
+    }
+    
+    public function delete($id) 
+    {
+        $row = $this->News_model->get_by_id($id);
+
+        if ($row) {
+            $this->News_model->delete($id);
+            $this->session->set_flashdata('message', 'Delete Record Success');
+            redirect(site_url('news'));
+        } else {
+            $this->session->set_flashdata('message', 'Record Not Found');
+            redirect(site_url('news'));
+        }
+    }
+
+    public function _rules() 
+    {
+	$this->form_validation->set_rules('title', 'title', 'trim|required');
+	$this->form_validation->set_rules('slug', 'slug', 'trim|required');
+	$this->form_validation->set_rules('text', 'text', 'trim|required');
+
+	$this->form_validation->set_rules('id', 'id', 'trim');
+	$this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
+    }
+
+    public function excel()
+    {
+        $this->load->helper('exportexcel');
+        $namaFile = "news.xls";
+        $judul = "news";
+        $tablehead = 0;
+        $tablebody = 1;
+        $nourut = 1;
+        //penulisan header
+        header("Pragma: public");
+        header("Expires: 0");
+        header("Cache-Control: must-revalidate, post-check=0,pre-check=0");
+        header("Content-Type: application/force-download");
+        header("Content-Type: application/octet-stream");
+        header("Content-Type: application/download");
+        header("Content-Disposition: attachment;filename=" . $namaFile . "");
+        header("Content-Transfer-Encoding: binary ");
+
+        xlsBOF();
+
+        $kolomhead = 0;
+        xlsWriteLabel($tablehead, $kolomhead++, "No");
+	xlsWriteLabel($tablehead, $kolomhead++, "Title");
+	xlsWriteLabel($tablehead, $kolomhead++, "Slug");
+	xlsWriteLabel($tablehead, $kolomhead++, "Text");
+
+	foreach ($this->News_model->get_all() as $data) {
+            $kolombody = 0;
+
+            //ubah xlsWriteLabel menjadi xlsWriteNumber untuk kolom numeric
+            xlsWriteNumber($tablebody, $kolombody++, $nourut);
+	    xlsWriteLabel($tablebody, $kolombody++, $data->title);
+	    xlsWriteLabel($tablebody, $kolombody++, $data->slug);
+	    xlsWriteLabel($tablebody, $kolombody++, $data->text);
+
+	    $tablebody++;
+            $nourut++;
+        }
+
+        xlsEOF();
+        exit();
+    }
+
+    public function word()
+    {
+        header("Content-type: application/vnd.ms-word");
+        header("Content-Disposition: attachment;Filename=news.doc");
+
+        $data = array(
+            'news_data' => $this->News_model->get_all(),
+            'start' => 0
+        );
+        
+        $this->load->view('news/news_doc',$data);
+    }
+
 }
+
+/* End of file News.php */
+/* Location: ./application/controllers/News.php */
+/* Please DO NOT modify this information : */
+/* Generated by Harviacode Codeigniter CRUD Generator 2019-02-07 09:31:14 */
+/* http://harviacode.com */
