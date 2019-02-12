5b4d2c804969c28c620b2d28e5def03fd79f1865
Maurits Seelen <mauritsseelen@gmail.com>
2019-02-12 17:18:32 +0100

    broodjes app

--- ci/application/controllers/Pages.php
+++ ci/application/controllers/Pages.php
@@ -5,12 +5,11 @@ class Pages extends CI_Controller {
     {
             if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
             {
-                    // Whoops, we don't have a page for that!
                     show_404();
             }
     
             $data['title'] = ucfirst($page); // Capitalize the first letter
-            $data['naam'] = "Massimo";
+            $data['naam'] = "Maurits";
             $this->load->view('templates/header', $data);
             $this->load->view('pages/'.$page, $data);
             $this->load->view('templates/footer', $data);
