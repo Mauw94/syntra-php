eab6b971b0b868ea726f4cfe23c54d211aedc63b
Maurits Seelen <mauritsseelen@gmail.com>
2019-03-31 14:11:19 +0200

    .

--- eindproef_ci/application/controllers/Home.php
+++ eindproef_ci/application/controllers/Home.php
@@ -12,7 +12,7 @@ class Home extends CI_Controller {
         $data['title'] = 'Home page';
 
         $this->load->view('templates/header');
-        $this->load->view('home/index', $data);
+        $this->load->view('home/home', $data);
         $this->load->view('templates/footer');
     }
 }
