34129773e3cc3a438ea05e6ecd103e50d464524f
Maurits Seelen <mauritsseelen@gmail.com>
2019-04-28 17:07:34 +0200

    company profile setup page start

--- eindproef_ci/application/controllers/Company.php
+++ eindproef_ci/application/controllers/Company.php
@@ -18,8 +18,18 @@ class Company extends CI_Controller {
 
     function index()
     {
+        $this->data = array(
+            'title' => 'Setup',
+            'action' => site_url('company/save_company'),
+            'userid' => $this->session->userdata('company')['user_id']
+        );
         $this->load->view('templates/header_main');
-        $this->load->view('company/setup_profile');
+        $this->load->view('company/setup_profile', $this->data);
         $this->load->view('templates/footer');
+    }
+
+    function save_company()
+    {
+
     }
 }
