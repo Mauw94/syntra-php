9a795bb2566b86e4cb608d3c7ef50582739708ee
Maurits Seelen <mauritsseelen@gmail.com>
2019-05-17 11:41:03 +0200

    fixed collapse

--- eindproef_ci/application/views/company/landing.php
+++ eindproef_ci/application/views/company/landing.php
@@ -8,16 +8,17 @@
 
     <div class="row"  style="margin-top: 50px;">
         <!-- iterate over projects and display in cards !-->
-        <?php foreach($projects as $project) { ?>
+        <?php 
+        foreach($projects as $project) { ?>
         <div class="col-md-3">
             <div class="card card-body">
                 <h5 class="card-title"><?php echo $project->name;?> </h5>
                 <p class="card-text"><?php echo $project->title;?> </p>
                 <hr>
-                <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                <a class="btn btn-primary" data-toggle="collapse" href="#collapse<?php echo $project->id;?>" role="button" aria-expanded="false" aria-controls="collapse<?php echo $project->id;?>">
                     View details</a>                                 
             </div>
-            <div class="collapse" id="collapseExample">
+            <div class="collapse" id="collapse<?php echo $project->id;?>">
                 <div class="card card-body">
                     <?php echo $project->description; ?>
                 </div>
